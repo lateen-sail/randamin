b0ae0511ae74ead7b0a64ac74c991928e39a49bf
miho <paglinawan0520@gmail.com>
2026-02-27 20:41:29 +0900

    remove: Book と Archive コンポーネント

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
-import { Books } from './Books'
 import { Words } from './Words'
-import { doArchive } from './Archive'
 import { doPost } from './Post'
 
 declare const global: {
   [x: string]: unknown
 }
 
-global.Books = Books
 global.Words = Words
-global.doArchive = doArchive
 global.doPost = doPost
